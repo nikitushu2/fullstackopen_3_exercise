ffa45b82d1236a56548bfb8a0625a31fcc04efb7
Nikita Shub <nikitushu2@gmail.com>
2024-11-22 15:17:20 +0200

    add frontend files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
